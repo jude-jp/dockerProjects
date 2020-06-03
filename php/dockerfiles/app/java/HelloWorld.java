3b8b57f273a0768c26c19c81d8857c0be997d592
Jude <jude_340@hotmail.fr>
2020-06-02 21:32:48 -0400

    refactoring

--- php/dockerfiles/app/java/HelloWorld.java
+++ php/dockerfiles/app/java/HelloWorld.java
@@ -3,6 +3,6 @@ package app.java;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello world !");
+        System.out.println("Hello world !\n");
     }
 }
