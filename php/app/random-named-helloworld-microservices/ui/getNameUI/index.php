561c2d14f48bb35a954b8cedafabd0116be62bab
Jude <jude_340@hotmail.fr>
2020-06-18 23:45:48 -0400

    changing names

---
+++ php/app/random-named-helloworld-microservices/ui/getNameUI/index.php
@@ -0,0 +1,19 @@
+<?php
+define("API_GETNAME_NAME", $_ENV['API_GETNAME_NAME']);
+
+$url = "http://" . API_GETNAME_NAME . "/users/firstname";
+
+$response = file_get_contents($url);
+
+if ($response != FALSE) {
+    $results = json_decode($response, true);
+
+    $resultsSize = count($results);
+    $randomNumber = rand(0, $resultsSize-1);
+    
+    echo "hello world " . $results[$randomNumber]["firstname"];
+} else {
+    echo "API non disponible...";
+}
+
+?>
